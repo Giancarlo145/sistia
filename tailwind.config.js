3747dedc821e977ed56b2942d2fd3bcbaec64e01
Giancarlo145 <giancarlo145@gmail.com>
2025-09-30 20:20:09 -0600

    Versión inicial del portafolio

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], // Busca en todos los archivos .html y .js de la carpeta raíz
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
